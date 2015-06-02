b138a7842ef1f959af2a8eade8566af70496ac48
Michael Zoech <maik@pixel.local>
2015-06-01 20:24:34 -0700

    Initial Commit

---
+++ AssertFlow/AssertFlow.h
@@ -0,0 +1,19 @@
+//
+//  AssertFlow.h
+//  AssertFlow
+//
+//  Created by Michael Zoech on 6/1/15.
+//  Copyright (c) 2015 Michael Zoech. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AssertFlow.
+FOUNDATION_EXPORT double AssertFlowVersionNumber;
+
+//! Project version string for AssertFlow.
+FOUNDATION_EXPORT const unsigned char AssertFlowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssertFlow/PublicHeader.h>
+
+

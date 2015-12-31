8379a0d35cb3cad1978ff07ca9106ec9ecf8992c
Michael Zoech <michi.zoech@gmail.com>
2015-12-30 16:27:21 -0800

    Put assertThat functions next to used type

--- Sources/Matcher/DictionaryMatcher.swift
+++ Sources/Matcher/DictionaryMatcher.swift
@@ -6,24 +6,26 @@ public class DictionaryMatcher<K: Hashable,V> : Matcher<Dictionary<K,V>> {
     public override init(actual: MatchInfo<Element>) {
         super.init(actual: actual)
     }
+}
+
+public extension DictionaryMatcher {
     
-    public func containsKey(expected: K) -> Self {
+    public func containsKey(key: K) -> Self {
         if unpack () {
-            if actual[expected] == nil {
-                fail("Expected dictionary to contain key:", expected: expected, actualMsg: "But key not found in:", actual: actual)
+            if actual[key] == nil {
+                fail("Expected dictionary to contain key:", expected: key, actualMsg: "But key not found in:", actual: actual)
             }
         }
         return self
     }
+}
+
+extension DictionaryMatcher where V: Equatable {
     
-    public func containsKey<X: Equatable>(key: K, value: X) -> Self{
+    public func containsKey(key: K, value: V) -> Self{
         if unpack() {
             if let actualValue = actual[key] {
-                if let castedValue = actualValue as? X {
-                    if castedValue != value {
-                        fail("Expected dictionary to contain key \(key) with value \(value)")
-                    }
-                } else {
+                if actualValue != value {
                     fail("Expected dictionary to contain key \(key) with value \(value)")
                 }
             } else {
@@ -32,4 +34,8 @@ public class DictionaryMatcher<K: Hashable,V> : Matcher<Dictionary<K,V>> {
         }
         return self
     }
+}
+
+public func assertThat<K: Hashable, V>(actual: Dictionary<K,V>?, file: String = __FILE__, line: UInt = __LINE__) -> DictionaryMatcher<K,V> {
+    return DictionaryMatcher(actual: MatchInfo(actual: actual, file: file, line: line))
 }
--- Sources/Matcher/StringMatcher.swift
+++ Sources/Matcher/StringMatcher.swift
@@ -1,13 +1,15 @@
 
 import Foundation
 
-
 public class StringMatcher : Matcher<String> {
     
     public override init(actual: MatchInfo<Element>) {
         super.init(actual: actual)
     }
-    
+}
+
+public extension StringMatcher {
+
     public func contains(expected: String) -> Self {
         if unpack() {
             if actual.rangeOfString(expected) == nil {
@@ -16,4 +18,8 @@ public class StringMatcher : Matcher<String> {
         }
         return self
     }
+}
+
+public func assertThat(actual: String?, file: String = __FILE__, line: UInt = __LINE__) -> StringMatcher {
+    return StringMatcher(actual: MatchInfo(actual: actual, file: file, line: line))
 }
